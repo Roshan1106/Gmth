2b67bc5ea0a74154531427a8fb9fe73dada4eaf0
rishikesh2004 <beastrishi2004@gmail.com>
2023-06-29 16:33:47 +0530

    routes

--- resources/views/home.php
+++ resources/views/home.php
@@ -6,6 +6,8 @@
     <title>Document</title>
 </head>
 <body>
-
+    <a href="<?=route('name2',['id'=>1,'age'=>1])?>">alkdj</a>
+    <a href="<?=route('name2',['id'=>2,'age'=>2])?>">;sdjkj</a>
+    <a href="<?=route('name2',['id'=>3])?>">kdn;sad</a>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,20 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::view('ho', 'home')->name('name2');
 
-Route::get('/',function(){
-    return "Hello";
-});
+Route::get('/name/{id}/{age?}',function($id,$age=null){
+    if($id==1&&$age==1)
+    {
+        return "rishi";
+    }
+    elseif($id==2&&$age==2)
+    {
+        return "roshan";
+    }
+    if($id==3)
+    {
+        return "pop";
+    }
+})->name('name2');
+
